4d4c7653250772e16d69a7a166199c6ad8198dbd
Mat Marquis <mat@matmarquis.com>
2011-12-23 15:49:21 -0500

    Additional fetchlink tests.

--- tests/unit/fetchlink/fetchlink_core.js
+++ tests/unit/fetchlink/fetchlink_core.js
@@ -6,7 +6,7 @@
 	
 	module( "Fetchlinks", {
 		setup: function(){
-			//
+		//	$.testHelper.openPage("#fetchlink-test1");
 		}
 	});
 
@@ -15,7 +15,7 @@
 		
 		$.testHelper.pageSequence([
 			function(){
-				$('.foo').trigger('click');
+				$('.standalone').trigger('click');
 			},
 			function(){
 				ok( targetContents !== $( '.loadinto' ).html() );
@@ -24,7 +24,7 @@
 		]);
 	});
 
-	asyncTest( "Clicking a fetchlink replaces previously loaded content with remote content.", function(){
+	asyncTest( "Clicking a grouped fetchlink loads remote content.", function(){
 		var targetContents = $( '.loadinto' ).html();
 		
 		$.testHelper.pageSequence([
@@ -38,23 +38,50 @@
 		]);
 	});
 	
+	asyncTest( "When no remote fragment is specified, the remote “page” element’s contents are pulled in instead.", function(){		
+		$.testHelper.pageSequence([
+			function(){
+				$('.remote-page').trigger('click');
+			},
+			function(){
+				var headerFirst = $('.loadinto').find('div:first').attr('data-nstest-role') === 'header',
+					footerLast = $('.loadinto').find('div:last').attr('data-nstest-role') === 'footer';
+					
+				ok( headerFirst && footerLast, "First and last items within the page wrapper match the first and last items in the target container." );
+				start();
+			}
+		]);
+	});
+	
 	asyncTest( "Elements are properly enhanced after being fetched.", function(){		
 		$.testHelper.pageSequence([
 			function(){
-				$('.baz').trigger('click');
-			},
-			function(){
-				ok( $('[data-nstest-role="header"]').hasClass( 'ui-header' ), "Page header is enhanced." );
-				ok( $('[data-nstest-role="content"]').hasClass( 'ui-content' ), "Page content is enhanced." );
-				ok( $('[data-nstest-role="footer"]').hasClass( 'ui-footer' ), "Page footer is enhanced." );
-				ok( $('[data-nstest-role="button"]').hasClass( 'ui-btn' ), "A link with a role of “button” is enhanced." );
-				ok( $('[data-nstest-role="slider"]').hasClass( 'ui-slider-switch' ), "Toggles are enhanced." );
-				ok( $('#test-slider').hasClass( 'ui-slider-input' ), "Slider widgets are enhanced." );
+				ok( $('.loadinto').find('[data-nstest-role="header"]').hasClass( 'ui-header' ), "Page header is enhanced." );
+				ok( $('.loadinto').find('[data-nstest-role="content"]').hasClass( 'ui-content' ), "Page content is enhanced." );
+				ok( $('.loadinto').find('[data-nstest-role="footer"]').hasClass( 'ui-footer' ), "Page footer is enhanced." );
+				ok( $('.loadinto').find('[data-nstest-role="button"]').hasClass( 'ui-btn' ), "A link with a role of “button” is enhanced." );
+				ok( $('.loadinto').find('[data-nstest-role="slider"]').hasClass( 'ui-slider-switch' ), "Toggles are enhanced." );
+				ok( $('.loadinto').find('#test-slider').hasClass( 'ui-slider-input' ), "Slider widgets are enhanced." );
 				
 				start();
 			}
 		]);
 	});
+	
+	asyncTest( "Fetchlinks within remote content function normally.", function(){		
+		var targetContents = $( '.secondtarget' ).html();
+		
+		$.testHelper.pageSequence([
+			function() {
+				$('.remote-fetchlink').trigger('click');
+			},
+			function(){
+				ok( $('.loadinto').html() !== targetContents, "Target container has been updated with remote content." );
+				start();
+			}
+		]);
+	});
+	
 	
 	
 
