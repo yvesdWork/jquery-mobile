84b567272b0cecac5ea5b37c38b7e9dcfa1773ef
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-01-12 01:48:15 +0200

    createDom: renaming to makeDom and using the solution proposed by arschmitz

--- js/jquery.mobile.createDom.js
+++ js/jquery.mobile.createDom.js
@@ -7,40 +7,15 @@ define( [ "jquery.mobile.core" ], function( $ ) {
 //>>excludeEnd("jqmBuildExclude");
 (function( $, undefined ) {
 
-//el can be one of two things:
-// 1. [ tagname, { attr: value, ... }, [ children ] ]
-// 2. "a string"
-var mkEl = function( el ) {
-		var ret, key, attr, children, child;
-
-		if ( $.type( el ) === "string" ) {
-			ret = document.createTextNode( el );
-		} else {
-			ret = document.createElement( el[ 0 ] );
-			for( attr in el[ 1 ] ) {
-				ret.setAttribute( attr, el[ 1 ][ attr ] );
-			}
-			children = mkChildren( el[ 2 ] );
-			for( child in children ) {
-				ret.appendChild( children[ child ] );
-			}
-		}
-
-		return ret;
-	},
-
-	mkChildren = function( c ) {
-		var ret = [], idx;
-
-		for ( idx in c ) {
-			ret.push( mkEl( c[ idx ] ) );
-		}
-
-		return ret;
-	};
-
-$.mobile.createDom = mkEl;
-$.mobile.createDomElements = mkChildren;
+$.mobile.makeDom = function( o ) {
+	var prop,
+		ele = document.createElement( o[ 0 ] );
+	for ( prop in o[ 1 ] ) {
+		ele.setAttribute( prop, o[ 1 ][ prop ] );
+	}
+	ele.innerHTML = o[ 2 ];
+	return ele;
+};
 
 })( jQuery );
 //>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
